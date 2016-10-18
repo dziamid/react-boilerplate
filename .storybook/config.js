a850b6107a218bede5cda7f37ea71f192fdcf976
Dziamid <dziamid@gmail.com>
2016-10-18 18:51:52 +0300

    use global storybook decorator for theming, use our own theme based on default

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,11 @@
-import { configure } from '@kadira/storybook';
+import React from 'react';
+import { configure, addDecorator } from '@kadira/storybook';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import theme from '../app/theme';
 
 function loadStories() {
   require('./index');
 }
-
+addDecorator((story) => <MuiThemeProvider muiTheme={getMuiTheme(theme)}>{story()}</MuiThemeProvider>);
 configure(loadStories, module);
--- app/components/Button/stories/index.js
+++ app/components/Button/stories/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Button from '../index';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 storiesOf('Button', module)
-  .addDecorator((story) => <MuiThemeProvider>{story()}</MuiThemeProvider>)
+
   .add('Raised buttons', () => (
     <div>
       <Button onClick={action('clicked')} label="Default" default />
