7e8437da8a7e7af1e223343362236902b4c07e89
Dziamid <dziamid@gmail.com>
2016-10-21 13:50:42 +0300

    example of using modal for form submittion confirmation and displaying server validation error

--- app/components/SampleForm/index.js
+++ app/components/SampleForm/index.js
@@ -1,49 +1,113 @@
-import React, { PropTypes } from 'react';
-import { Field, reduxForm, change } from 'redux-form';
+import React, { PropTypes, Component } from 'react';
+import { Field, reduxForm, change, SubmissionError } from 'redux-form';
 import TextField from 'components/TextField';
 import Button from 'components/Button';
 import Checkbox from 'components/Checkbox';
+import Dialog from 'components/Dialog';
 import styles from './styles.css';
 
+const sleep = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
 
-const SampleForm = (props) => {
-  const { handleSubmit, reset, submitting, dispatch, form } = props;
+class SampleForm extends Component {
+  constructor() {
+    super();
 
-  return (
-    <div>
-      <h1>Sample form with ReduxForm and MUI components</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Field name="field1" component={TextField} type="text" label="Label on the top" />
-        </div>
-        <div>
-          <Field
-            name="field2"
-            component={TextField}
-            type="text"
-            withClear
-            onClear={() => dispatch(change(form, 'field2', ''))}
-            label="With clear button"
-          />
-        </div>
-        <div>
-          <Field
-            name="field3"
-            component={Checkbox}
-            label="Checkbox with label on the right"
-            labelPosition="right"
-          />
-        </div>
+    this.state = {
+      showConfirmDialog: false,
+      submittingValues: {},
+    };
+    this.closeConfirmDialog = this.closeConfirmDialog.bind(this);
+    this.confirmSubmit = this.confirmSubmit.bind(this);
+    this.submit = this.submit.bind(this);
+  }
 
-        <div className={styles.actionButtons}>
-          <Button type="submit" raised default onClick={reset} disabled={submitting}>Clear</Button>
-          <Button type="submit" raised primary onClick={handleSubmit} disabled={submitting}>Submit</Button>
-        </div>
+  closeConfirmDialog() {
+    this.setState({ showConfirmDialog: false });
+  }
 
-      </form>
-    </div>
-  );
-};
+  confirmSubmit(submittingValues) {
+    this.setState({
+      showConfirmDialog: true,
+      submittingValues,
+    });
+  }
+
+  submit() {
+    return sleep(500)
+      .then(() => {
+        this.closeConfirmDialog();
+
+        throw new SubmissionError({ field1: 'Server validation failed' });
+      });
+  }
+
+  render() {
+    const { handleSubmit, reset, submitting, dispatch, form } = this.props;
+    const dialogActions = [
+      <Button label="Cancel" onClick={this.closeConfirmDialog} />,
+      <Button label="Submit" primary onClick={handleSubmit(this.submit)} />,
+    ];
+
+    const dialog = (
+      <Dialog
+        title="Are you sure?"
+        actions={dialogActions}
+        modal
+        open={this.state.showConfirmDialog}
+      >
+        <p>You are about to submit the following data to server:</p>
+        <pre>
+          {JSON.stringify(this.state.submittingValues, null, 4)}
+        </pre>
+      </Dialog>
+    );
+
+    return (
+      <div>
+        { dialog }
+
+        <h1>Sample form with ReduxForm and MUI components</h1>
+        <form >
+          <div>
+            <Field name="field1" component={TextField} type="text" label="Label on the top" />
+          </div>
+          <div>
+            <Field
+              name="field2"
+              component={TextField}
+              type="text"
+              withClear
+              onClear={() => dispatch(change(form, 'field2', ''))}
+              label="With clear button"
+            />
+          </div>
+          <div>
+            <Field
+              name="field3"
+              component={Checkbox}
+              label="Checkbox with label on the right"
+              labelPosition="right"
+            />
+          </div>
+
+          <div className={styles.actionButtons}>
+            <Button raised default onClick={reset} disabled={submitting}>Clear</Button>
+            <Button
+              type="submit"
+              raised primary
+              disabled={submitting}
+              onClick={handleSubmit(this.confirmSubmit)}
+            >
+              Submit
+            </Button>
+          </div>
+
+        </form>
+      </div>
+    );
+  }
+
+}
 
 SampleForm.propTypes = {
   handleSubmit: PropTypes.func,
--- app/components/SampleForm/stories/index.js
+++ app/components/SampleForm/stories/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { SubmissionError } from 'redux-form';
-import { storiesOf, action } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
 import SampleForm from '../index';
 
-const sleep = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
-
-const submit = (values) => {
-  action('form-submitted')(values);
-
-  return sleep(300)
-    .then(() => {
-      action('response-arrived')();
-      throw new SubmissionError({ field1: 'Server validation failed' });
-    });
-};
-
 storiesOf('SampleForm', module)
-  .add('default', () => (
-    <SampleForm onSubmit={action('form-submitted')} />
-  ))
-  .add('with async validation error', () => (
-    <SampleForm onSubmit={submit} />
+  .add('with server modal confirm and server validation error', () => (
+    <SampleForm />
   ));
