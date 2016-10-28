c4b76092a5273c82b09d9bcf59194b72021d2cd2
Dziamid <dziamid@gmail.com>
2016-10-28 20:49:54 +0300

    focus textfield on clear

--- app/components/TextField/index.js
+++ app/components/TextField/index.js
@@ -1,37 +1,61 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { default as MUITextField } from 'material-ui/TextField';
 import Button from 'components/Button';
 import styles from './styles.css';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-const TextField = (props) => {
-  const { input, label, onClear, withClear, labelPosition, meta: { touched, error }, ...other } = props;
-  const withLeftLabel = labelPosition === 'left';
-  const floatingLabel = !withLeftLabel ? label : undefined;
+class TextField extends Component {
+  constructor() {
+    super();
+    this.handleClear = this.handleClear.bind(this);
+  }
 
-  const rootClassnames = {
-    withLeftLabel,
-    withClear,
-  };
+  handleClear() {
+    this.props.onClear();
+    this.muiTextField.focus();
+  }
 
-  return (
-    <div className={cx(rootClassnames)}>
-      {withLeftLabel ? <label>{label}</label> : null}
+  render() {
+    const {
+      input,
+      label,
+      onClear, // eslint-disable-line no-unused-vars
+      withClear,
+      labelPosition,
+      meta: { touched, error },
+      ...other,
+    } = this.props;
 
-      <MUITextField
-        floatingLabelText={floatingLabel}
-        floatingLabelFixed={floatingLabel !== undefined}
-        errorText={touched && error}
-        {...input}
-        {...other}
-      />
+    const withLeftLabel = labelPosition === 'left';
+    const floatingLabel = !withLeftLabel ? label : undefined;
 
-      {withClear ? <Button onClick={onClear} className={styles.clearButton} icon="close" /> : null }
+    const rootClassnames = {
+      withLeftLabel,
+      withClear,
+    };
 
-    </div>
-  );
-};
+    return (
+      <div className={cx(rootClassnames)}>
+        {withLeftLabel ? <label>{label}</label> : null}
+
+        <MUITextField
+          ref={ref => (this.muiTextField = ref)}
+          floatingLabelText={floatingLabel}
+          floatingLabelFixed={floatingLabel !== undefined}
+          errorText={touched && error}
+          {...input}
+          {...other}
+        />
+
+        {withClear ? <Button onClick={this.handleClear} className={styles.clearButton} icon="close" /> : null }
+
+      </div>
+    );
+  }
+
+}
+
 
 TextField.propTypes = {
   input: PropTypes.object,
