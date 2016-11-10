775794f0fe7c05bc13e1d54277bc7a3baec35ae1
Dziamid <dziamid@gmail.com>
2016-11-10 15:43:13 +0300

    use mock Form in stories to show controlled example of TextField

--- app/components/TextField/stories/index.js
+++ app/components/TextField/stories/index.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import TextField from '../index';
 
-const parentProps = {
-  input: { name: 'field1' },
-  meta: {},
-};
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+    };
+  }
+
+  hanleChange(value) {
+    action('onChange')(value);
+    this.setState({ value });
+  }
+
+  handleClear() {
+    action('onClear')();
+    this.setState({ value: '' });
+  }
+
+  render() {
+    const props = {
+      input: {
+        onChange: e => this.hanleChange(e.target.value),
+        value: this.state.value,
+      },
+      meta: {},
+      onClear: () => this.handleClear(),
+    };
+    return (
+      <div>
+        {React.cloneElement(this.props.children, props)}
+      </div>
+    );
+  }
+}
+
 
 storiesOf('TextField', module)
+  .addDecorator(getStory => <Form>{getStory()}</Form>)
+
   .add('default', () => (
-    <TextField label="Label on the top" {...parentProps} />
+    <TextField label="Label on the top" />
   ))
   .add('with label on the left', () => (
-    <TextField label="Label on the left" labelPosition="left" {...parentProps} />
+    <TextField label="Label on the left" labelPosition="left" />
   ))
   .add('with clear icon', () => (
-    <TextField label="Label on the left" withClear onClear={action('will clear input')} {...parentProps} />
+    <TextField label="Label on the left" withClear onClear={action('will clear input')} />
   ));
