22818f1a6138c7bec75d900b4d9f575e0ea6164e
Alexey Molchan <alexeymolchan228@gmail.com>
2016-11-29 11:33:45 +0300

    changed width of list

--- app/components/InstructionsSidebar/stories/index.js
+++ app/components/InstructionsSidebar/stories/index.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@kadira/storybook';
 import InstructionSidebar from '../index';
 
 const listStyle = { padding: '0 0 15px', fontFamily: '"Lato", sans-serif', fontSize: '13px', fontWeight: 300, lineHeight: '17px', color: '#1F768A', letterSpacing: 0 };
-const ulStyle = { listStyle: 'none', padding: 0, margin: 0, width: '220px' };
+const ulStyle = { listStyle: 'none', padding: 0, margin: 0, width: '230px' };
 const footerInfo = {
   position: 'Senior UX/UI Designer',
   company: 'Airbnb HQ - San-Francisco',
@@ -15,7 +15,7 @@ const body = (
     <li style={listStyle}>Nunc non diam metus. Fusce ornare pretium sodales.</li>
     <li style={listStyle}>Vivamus semper, tortor vel efficitur.</li>
     <li style={listStyle}>Maecenas faucibus mollis interdum.</li>
-    <li style={listStyle}>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras mattis consectetur amet fermentum</li>
+    <li style={listStyle}>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras mattis consectetur pit amet fermentum.</li>
   </ul>
 );
 
