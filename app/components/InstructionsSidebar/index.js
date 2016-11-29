8fbd4cad6e5d3aa7a4613c9821155646966b9dfe
Alexey Molchan <alexeymolchan228@gmail.com>
2016-11-29 11:22:36 +0300

    added Instructions Sidebar component with and without footer

--- app/components/InstructionsSidebar/index.js
+++ app/components/InstructionsSidebar/index.js
@@ -1,3 +1,64 @@
-/**
- * Created by alexeymolchan on 28.11.16.
- */
+import React from 'react';
+import styles from './styles.css';
+
+export default class InstructionsSidebar extends React.Component {
+
+  render() {
+    const {
+      footer,
+      footerInfo,
+      body,
+    } = this.props;
+
+    const footerStyles = {
+      wrapper: { backgroundColor: '#DDF6F6', fontFamily: '"Lato", sans-serif', fontSize: '13px', lineHeight: '11px', padding: '0 0 30px' },
+      container: { display: 'flex', flexDirection: 'column', alignItems: 'flex-start', padding: '0 0 0 20px' },
+      title: { display: 'inline-block', fontSize: '13px', textTransform: 'uppercase', color: '#1f768a', fontWeight: 400, margin: '15px 0 20px', lineHeight: '11px' },
+      ul: { listStyle: 'none', padding: 0, margin: 0, width: '200px' },
+      liPosition: { color: '#757575', fontWeight: 400, margin: '0 0 15px' },
+      li: { color: '#757575', fontWeight: 300, margin: '0 0 15px' },
+    };
+
+    const ftr = (
+      <div style={footerStyles.wrapper}>
+        <div style={footerStyles.container}>
+          <p style={footerStyles.title}>Summary</p>
+          <ul style={footerStyles.ul}>
+            <li style={footerStyles.liPosition}>{footerInfo.position ? footerInfo.position : null}</li>
+            <li style={footerStyles.li}>{footerInfo.company ? footerInfo.company : null}</li>
+            <li style={footerStyles.li}>{footerInfo.workflow ? footerInfo.workflow : null}</li>
+          </ul>
+        </div>
+      </div>
+    );
+
+    let placeholder = 'http://placehold.it/115x115/1AC5C0/ffffff';
+    let titleText = 'Tips for a winning description';
+    let containerStyle = styles.container;
+    let iconStyle = styles.icon;
+    let titleStyle = styles.title;
+
+    if (footer) {
+      placeholder = 'http://placehold.it/40x40/1AC5C0/ffffff';
+      containerStyle = styles.container_with_f;
+      iconStyle = styles.icon_with_f;
+      titleText = 'Optimize Job Position Requirements';
+      titleStyle = styles.title_with_p;
+    }
+
+    return (
+      <div className={styles.wrapper}>
+        <div className={containerStyle}>
+          <div>
+            <img className={iconStyle} src={placeholder} alt="icon" />
+          </div>
+          <div className={styles.container_bot}>
+            <h1 className={titleStyle}>{titleText}</h1>
+            { body }
+          </div>
+        </div>
+        { footer ? ftr : null}
+      </div>
+    );
+  }
+}
--- app/components/InstructionsSidebar/stories/index.js
+++ app/components/InstructionsSidebar/stories/index.js
@@ -1,3 +1,29 @@
-/**
- * Created by alexeymolchan on 28.11.16.
- */
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import InstructionSidebar from '../index';
+
+const listStyle = { padding: '0 0 10px', fontFamily: '"Lato", sans-serif', fontSize: '13px', fontWeight: 300, lineHeight: '18px', color: '#1F768A', letterSpacing: 0 };
+const ulStyle = { listStyle: 'none', padding: 0, margin: 0, width: '220px' };
+const footerInfo = {
+  position: 'Senior UX/UI Designer',
+  company: 'Airbnb HQ - San-Francisco',
+  workflow: 'Hiring Workflow 1',
+
+};
+const body = (
+  <ul style={ulStyle}>
+    <li style={listStyle}>Nunc non diam metus. Fusce ornare pretium sodales.</li>
+    <li style={listStyle}>Vivamus semper, tortor vel efficitur.</li>
+    <li style={listStyle}>Maecenas faucibus mollis interdum.</li>
+    <li style={listStyle}>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras mattis consectetur purus sit amet fermentum</li>
+  </ul>
+);
+
+storiesOf('InstructionSidebar', module)
+
+  .add('without footer', () => (
+    <InstructionSidebar body={body} footerInfo />
+  ))
+  .add('with footer', () => (
+    <InstructionSidebar body={body} footer footerInfo={footerInfo} />
+  ));
