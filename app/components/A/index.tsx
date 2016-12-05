ced3170d6f8383d88d6cca9e8388661cae447ac1
Dziamid <dziamid@gmail.com>
2016-12-05 19:17:49 +0300

    convert A component to ts without spread operator

---
+++ app/components/A/index.tsx
@@ -0,0 +1,35 @@
+/**
+ * A link to a certain page, an anchor tag
+ */
+
+import * as React from 'react';
+import {omit} from 'lodash';
+const styles = require('./styles.css');
+
+interface IAProps extends React.ClassAttributes<A> {
+  children: React.ReactNode,
+  className?: string,
+  href: string,
+  target?: string,
+}
+
+class A extends React.Component<IAProps, {}> {
+  public render() {
+    const {className, children, href, target} = this.props;
+    const other = omit(this.props, ['className', 'children', 'href', 'target']);
+
+    return (
+      <a
+        href={href}
+        target={target}
+        className={className || styles.link}
+        {...other}
+      >
+        { children }
+      </a>
+    );
+  }
+
+}
+
+export default A;
