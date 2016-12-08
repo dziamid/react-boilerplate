e8d356083f48227bafcdde133f18aa65135c0e53
Dziamid <dziamid@gmail.com>
2016-12-08 18:32:53 +0300

    code polish, console.log final webpack config used in storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,20 +2,24 @@ const path = require('path');
 const devConfig = require('../internals/webpack/webpack.dev.babel');
 
 const rootDir = path.resolve('./app');
-console.log(`rootDir: ${rootDir}`);
-console.log(`resolve:`, devConfig.resolve);
-
-module.exports = {
+const resolve = Object.assign({}, devConfig.resolve, { root: rootDir });
+const loaders = Object.assign([], devConfig.module.loaders);
+const config = {
   devtool: devConfig.devtool,
   cssLoaders: devConfig.cssLoaders,
   plugins: [
     // your custom plugins
   ],
-  resolve: Object.assign({}, devConfig.resolve, {root: rootDir}),
+  resolve,
 
   module: {
-    loaders: Object.assign([], devConfig.module.loaders),
+    loaders,
   },
 
   postcss: devConfig.postcss
 };
+
+console.log(`rootDir: ${rootDir}`);
+console.log(`resolve:`, devConfig.resolve);
+
+module.exports = config;
--- app/components/SelectField/stories/index.js
+++ app/components/SelectField/stories/index.js
@@ -4,15 +4,18 @@ import SelectField from '../index';
 import MenuItem from 'components/MenuItem';
 import countries from 'components/common/stories/countries';
 
-const parentProps = () => ({
-  input: { onChange: action('onChange') },
+const props = () => ({
+  input: { onChange: action('onChange'), value: 'BY' },
 });
 
-const options = countries.map(c => <MenuItem key={c.code} value={c.code} primaryText={c.name} />);
+const children = () => countries.map(c => <MenuItem key={c.code} value={c.code} primaryText={c.name} />);
 
 storiesOf('SelectField', module)
   .add('default', () => (
-    <SelectField {...parentProps()} label="Select country" value="AU">
-      {options}
+    <SelectField
+      {...props()}
+      label="Select country"
+    >
+      {children()}
     </SelectField>
   ));
